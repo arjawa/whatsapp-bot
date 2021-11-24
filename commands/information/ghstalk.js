fba90ea3efb1d5dd16b8ee4b81f55fa841f4d83c
briexenv <briexenv@gmail.com>
2021-11-24 14:25:33 +0800

    fix github stalk

--- commands/information/ghstalk.js
+++ commands/information/ghstalk.js
@@ -7,7 +7,7 @@ module.exports = {
     utilisation: userbot.prefix + 'ghstalk <username>',
     
     async execute(m) {
-        let { args } = data
+        let { conn, args } = data
         username = args[0]
         if (!username) return m.reply(`contoh: ${userbot.prefix}ghstalk briexenv`)
         try {
@@ -16,7 +16,7 @@ module.exports = {
             let bio = gh.bio !== null ? `Bio : ${gh.bio}\n` : ''
             let email = gh.email !== null ? `Email : ${gh.email}\n` : ''
             let teks = `*GITHUB*\nName : ${gh.name} (@${gh.login})\n${email + bio}Followers : ${gh.followers}\nFollowing : ${gh.following}\nPublic Repos : ${gh.public_repos}`
-            conn.sendButtonImg(m.chat, teks.trim(), await Ft.getBuffer(gh.avatar_url), userbot.packname, 'FOLLOW', 'ghfollow '+gh.html_url)
+            conn.sendButtonImg(m.chat, teks.trim(), gh.avatar_url, userbot.packname, 'FOLLOW', 'ghfollow '+gh.html_url)
         } catch (e) {
             if (e.request) return m.reply('Username tidak ditemukan, silahkan tunggu beberapa saat dan coba lagi')
         }
